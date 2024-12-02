390d5f6a1415b508657e1b315f7571186ae432d6
Bektas Keldibayev <bektas.keldibayev@nu.edu.kz>
2024-12-02 09:51:37 +0500

    add chat to buyer page

--- api/buyer/getChats.js
+++ api/buyer/getChats.js
@@ -4,13 +4,9 @@ import { BACKEND_URL } from "../config";
 export const getChatList = async () => {
   try {
     const token = await getToken();
-    const response = await axios.get(
-      `${BACKEND_URL}/chat/conversations`,
-      {},
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
+    const response = await axios.get(`${BACKEND_URL}/chat/conversations`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (err) {
     console.error("error getting chat list...");
--- api/buyer/getMessages.js
+++ api/buyer/getMessages.js
@@ -4,10 +4,9 @@ import { BACKEND_URL } from "../config";
 export const getMessages = async (conversationID) => {
   try {
     const token = await getToken();
-    console.log(conversationID);
+    console.log("convresation id in getMessages:", conversationID);
     const response = await axios.get(
       `${BACKEND_URL}/chat/conversations/${conversationID}/messages`,
-      {},
       {
         headers: { Authorization: `Bearer ${token}` },
       }
